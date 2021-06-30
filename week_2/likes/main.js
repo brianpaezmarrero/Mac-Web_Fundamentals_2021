d7ed117b2084fbd2dbad94186a721eab1bb66e44
Brian Paez <brianpaezmarrero@gmail.com>
2021-06-29 22:23:24 -0700

    done 6

---
+++ week_2/likes/main.js
@@ -0,0 +1,35 @@
+
+
+var count1 = 9;
+var countLike1 = document.querySelector("#like1");
+console.log(countLike1)
+function likesCounter1(){
+    count1++;
+    console.log(count1);
+    countLike1.innerText = count1 + " like(s)";
+}
+
+
+// SECOND LIKE------------------------------//
+// -----------------------------------------//
+
+var count2 = 12;
+var countLike2 = document.querySelector("#like2");
+console.log(countLike2)
+function likesCounter2(){
+    count2++;
+    console.log(count2);
+    countLike2.innerText = count2 + " like(s)";
+}
+
+
+// THIRD
+
+var count3 = 9;
+var countLike3 = document.querySelector("#like3");
+console.log(countLike3)
+function likesCounter3(){
+    count3++;
+    console.log(count3);
+    countLike3.innerText = count3 + " like(s)";
+}
